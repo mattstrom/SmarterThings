29be0278debe6a137e7fe27f0ab530b578968b45
Matt Strom <strom.matt@gmail.com>
2020-02-22 12:21:06 -0800

    fix: intercept only requests to own api

--- packages/ui/src/app/modules/auth/services/token.interceptor.ts
+++ packages/ui/src/app/modules/auth/services/token.interceptor.ts
@@ -1,21 +1,27 @@
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { ApiUrlToken } from '../../../tokens';
 import { AuthService } from './auth.service';
 
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-	constructor(public auth: AuthService) {}
+	constructor(
+		public auth: AuthService,
+		@Inject(ApiUrlToken) private apiUrl: string
+	) {}
 
 	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		request = request.clone({
-			setHeaders: {
-				Authorization: `Bearer ${this.auth.getToken()}`
-			}
-		});
+		if (request.url.startsWith(this.apiUrl)) {
+			request = request.clone({
+				setHeaders: {
+					Authorization: `Bearer ${this.auth.getToken()}`
+				}
+			});
+		}
 
 		return next.handle(request);
 	}
